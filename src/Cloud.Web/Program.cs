62098841001de1d45e820462a0e3008ea88437db
Marek Olšák <olsak.marek@outlook.cz>
2023-09-20 21:25:43 +0200

    Added razor pages, blazor server side, middleware configuration, razor pages etc.

--- src/Cloud.Web/Program.cs
+++ src/Cloud.Web/Program.cs
@@ -1,7 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var services = builder.Services;
+var config = builder.Configuration;
+var env = builder.Environment;
+
+services.AddRazorPages();
+services.AddServerSideBlazor();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (env.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
