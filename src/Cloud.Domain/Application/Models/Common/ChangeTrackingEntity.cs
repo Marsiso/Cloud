df246f1a1e7584ff7f2bcb6eb5c9bee0b31cfba1
Marek Olšák <olsak.marek@outlook.cz>
2023-09-20 21:52:03 +0200

    Add a user entity and a base classes for the entities.

---
+++ src/Cloud.Domain/Application/Models/Common/ChangeTrackingEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Cloud.Domain.Application.Models.Common;
+
+public abstract class ChangeTrackingEntity : EntityBase
+{
+    public int? CreatedBy { get; set; }
+    public int? UpdatedBy { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime DateUpdated { get; set; }
+}
---
+++ src/Cloud.Domain/Application/Models/Common/EntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Cloud.Domain.Application.Models.Common;
+
+public abstract class EntityBase
+{
+    public int ID { get; set; }
+    public bool IsActive { get; set; }
+}
