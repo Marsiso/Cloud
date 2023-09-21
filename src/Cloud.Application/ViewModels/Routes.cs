f87fe9f2bd7409dede0cbeb15235310f356093ad
Marek Olšák <olsak.marek@outlook.cz>
2023-09-21 16:34:00 +0200

    Added login, logout and register pages.

---
+++ src/Cloud.Application/ViewModels/Routes.cs
@@ -0,0 +1,20 @@
+﻿namespace Cloud.Application.ViewModels;
+
+public static class Routes
+{
+    public const string Root = "/";
+
+    public static class Home
+    {
+        public const string Base = "/";
+        public const string Index = Base;
+    }
+
+    public static class Authentication
+    {
+        public const string Base = $"/auth";
+        public const string Login = $"{Base}/login";
+        public const string Logout = $"{Base}/logout";
+        public const string Register = $"{Base}/register";
+    }
+}
